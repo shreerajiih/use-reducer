4335374f4acdda74e946d46525298f4a734dcdf6
shreerajiih <shreeraj.iihglobal@gmail.com>
2025-03-20 16:19:21 +0530

    useHistory

--- src/UseLocationCompo.jsx
+++ src/UseLocationCompo.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation ,useHistory} from 'react-router-dom'
 
 const UseLocationCompo = () => {
     const location = useLocation();
+    const history = useHistory();
     console.log("Location is",location);
   
     console.log("Path is ",location.pathname);
     console.log("qUERY sTRING IS ",location.search);
   
     console.log("Count is ", count)
+
+    // history.goBack();  ----> used to go back path
+    // history.push("/");  ----> used to go to home page
   
   return (
     <div>
-      
+      <button onClick={()=>history.goBack()}>Go Back</button>
     </div>
   )
 }
